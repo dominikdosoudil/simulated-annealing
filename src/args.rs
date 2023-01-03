9f7dcb06d0cf64f3d24f9a2ff9127ffe668f437a
Dominik Dosoudil <dosoudom@fit.cvut.cz>
2023-01-03 20:31:47 +0100

    fix conflictiong short ops

--- src/args.rs
+++ src/args.rs
@@ -8,27 +8,21 @@ pub(crate) enum TailCutMethod {
 
 #[derive(Parser, Debug)]
 pub(crate) struct Args {
-    #[arg(short, long, default_value_t = 300)]
-    pub(crate) tries_max: u32,
-
-    #[arg(short, long, default_value_t = 10)]
-    pub(crate) flips_max: u32,
-
     #[arg(short, long)]
     pub(crate) input: String,
 
     #[arg(short, long, default_value_t = 0.995)]
     pub(crate) cooling_ratio: f64,
 
-    #[arg(short, long, default_value_t = 5.)]
-    pub(crate) min_temperature: f64,
+    #[arg(short, long, default_value_t = 2)]
+    pub(crate) min_temperature: i32,
 
     #[arg(short, long, default_value_t = 4)]
     pub(crate) penalty_multiplier: i64,
 
-    #[arg(short, long, default_value_t = 2500)]
+    #[arg(long, default_value_t = 3000)]
     pub(crate) tail_cut_length: usize,
 
-    #[arg(short, long, value_enum, default_value_t = TailCutMethod::RelativeDeviation)]
+    #[arg(long, value_enum, default_value_t = TailCutMethod::RelativeDeviation)]
     pub(crate) tail_cut_method: TailCutMethod,
 }
